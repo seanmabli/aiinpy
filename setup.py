448c09376bc65f831e67f539b738b8f41dbefa23
Sean-Mabli <smdrone1@gmail.com>
2021-04-19 17:41:19 -0400

    _

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='aiinpy',
       version='0.0.0',
       description='A simple artificial intelligence library',
-      author='Sean Mabli',
       license='MIT',
       packages=['aiinpy'],
-      install_requires=[numpy'])
+      install_requires=['numpy'])
