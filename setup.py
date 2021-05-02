97e40856f77d8a9dc3667828c20eefb5ea4d54cb
Sean-Mabli <smdrone1@gmail.com>
2021-05-02 18:40:55 -0400

    idk

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='aiinpy',
-      version='0.0.4',
+      version='0.0.5',
       description='A simple artificial intelligence library',
       license='MIT',
       packages=['aiinpy'],
