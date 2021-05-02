5f6e00098cf1eab061b11da605365d2b60031c31
Sean-Mabli <smdrone1@gmail.com>
2021-05-02 18:18:28 -0400

    idk

--- examples/RNN_Example.py
+++ examples/RNN_Example.py
@@ -1,7 +1,10 @@
 import numpy as np
+import aiinpy as ai
 
 PositiveComments = open("C:\\Users\\smdro\\Downloads\\archive\\sentence_polarity\\rt-polarity.pos", "r")
 NegativeComments = open("C:\\Users\\smdro\\Downloads\\archive\\sentence_polarity\\rt-polarity.neg", "r")
 Comments = str(PositiveComments.read() + NegativeComments.read())
 Comments = np.array(Comments.splitlines())
-np.random.shuffle(Comments)
+
+TrainingDataUniqueWords = list(set([w for Sentence in Comments for w in Sentence.split(' ')]))
+Rnn = ai.RNN(len(TrainingDataUniqueWords), 2, "ManyToOne")
