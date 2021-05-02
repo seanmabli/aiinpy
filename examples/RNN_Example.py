e6e62996b0b09a78baeaf767593a778da4a347f2
Sean-Mabli <smdrone1@gmail.com>
2021-05-02 19:49:03 -0400

    idk

--- examples/RNN_Example.py
+++ examples/RNN_Example.py
@@ -6,5 +6,21 @@ NegativeComments = open("C:\\Users\\smdro\\Downloads\\archive\\sentence_polarity
 Comments = str(PositiveComments.read() + NegativeComments.read())
 Comments = np.array(Comments.splitlines())
 
-TrainingDataUniqueWords = list(set([w for Sentence in Comments for w in Sentence.split(' ')]))
-Rnn = ai.RNN(len(TrainingDataUniqueWords), 2, "ManyToOne")
+UniqueWords = list(set([w for Sentence in Comments for w in Sentence.split(' ')]))
+Rnn = ai.RNN(len(UniqueWords), 2, "ManyToOne")
+
+for Generation in range(1):
+  Random = np.random.randint(0, len(Comments))
+
+  InputSentenceSplit = list(Comments[Random].split(' '))
+  Input = np.zeros((len(InputSentenceSplit), len(UniqueWords)))
+  for i in range(len(InputSentenceSplit)):
+    Input[i, UniqueWords.index(InputSentenceSplit[i])] = 1
+
+  Output = Rnn.ForwardProp(Input)
+
+  RealOutput = np.array([1, 0]) if Random < 5331 else np.array([0, 1])
+
+  OutputError = RealOutput - Output
+
+  Rnn.BackProp(OutputError)
