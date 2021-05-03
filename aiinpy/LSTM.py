92aa2eace306f871524feed4d8cd1b9d77cac6ba
Sean-Mabli <smdrone1@gmail.com>
2021-05-02 21:02:34 -0400

    idk

---
+++ aiinpy/LSTM.py
@@ -0,0 +1,6 @@
+import numpy as np
+from .ActivationFunctions import Sigmoid, DerivativeOfSigmoid, StableSoftMax, DerivativeOfStableSoftMax, ReLU, DerivativeOfReLU, Tanh, DerivativeOfTanh
+
+class LSTM:
+  def __init__(self):
+    
--- aiinpy/RNN_Upload_Dataset.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-PositiveComments = open("C:\\Users\\smdro\\Downloads\\archive\\sentence_polarity\\rt-polarity.pos", "r")
-NegativeComments = open("C:\\Users\\smdro\\Downloads\\archive\\sentence_polarity\\rt-polarity.neg", "r")
-Comments = str(PositiveComments.read() + NegativeComments.read())
-Comments = np.array(Comments.splitlines())
-np.random.shuffle(Comments)
