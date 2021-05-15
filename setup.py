86a1a35d9924ca8d58ff947f875511c212df6a0f
Sean-Mabli <smdrone1@gmail.com>
2021-05-15 11:47:11 -0400

    publish 0.0.8

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='aiinpy',
-      version='0.0.7',
-      description='A simple artificial intelligence library',
+      version='0.0.8',
+      description='An artificial intelligence library',
       license='MIT',
       packages=['aiinpy'],
       install_requires=['numpy']
