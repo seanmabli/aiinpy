0e3abe69f80b32e7acb57c44b9cdd57d911ee465
Sean-Mabli <smdrone1@gmail.com>
2021-05-18 21:41:43 -0400

    activation functions and dropout

--- example/GAN/ActivationFunctions.py
+++ example/GAN/ActivationFunctions.py
@@ -1,32 +1,32 @@
 import numpy as np
 
 class Sigmoid:
-  def __init__(self, Input):
+  def Sigmoid(self, Input):
     return 1 / (1 + np.exp(-Input))
   def Derivative(self, Input):
     return Input * (1 - Input)
 
 class Tanh:
-  def __init__(Input):
+  def Tanh(self, Input):
     return np.tanh(Input)
-  def Derivative(Input):
+  def Derivative(self, Input):
     return 1 - np.square(Input)
 
 class ReLU:
-  def __init__(Input):
+  def ReLU(self, Input):
     Output = np.maximum(0, Input)
     return Output
-  def Derivative(Input):
-    Equation = np.vectorize(EquationForDerivativeOfReLU, otypes=[float])
+  def Derivative(self, Input):
+    Equation = np.vectorize(self.EquationForDerivative, otypes=[float])
     return Equation(Input)
-  def EquationForDerivative(Input):
+  def EquationForDerivative(self, Input):
     return 0 if (Input <= 0) else 1
 
 class LeakyReLU:
-  def __init__(self, Input):
+  def LeakyReLU(self, Input):
     return np.maximum(0.01 * Input, Input)
   def Derivative(self, Input):
-    Equation = np.vectorize(EquationForDerivativeOfLeakyReLU, otypes=[float])
+    Equation = np.vectorize(self.EquationForDerivative, otypes=[float])
     return Equation(Input)
   def EquationForDerivative(self, Input):
     if self.Slope is None:
@@ -35,10 +35,10 @@ class LeakyReLU:
       return self.Slope if (Input < 0) else 1
 
 class StableSoftMax:
-  def __init__(Input):
+  def StableSoftMax(self, Input):
     return np.exp(Input - np.max(Input)) / np.sum(np.exp(Input - np.max(Input)))
-  def Derivative(Input):
-    Equation = np.vectorize(EquationForDerivativeOfStableSoftMax, otypes=[float])
+  def Derivative(self, Input):
+    Equation = np.vectorize(self.EquationForDerivativeOfStableSoftMax, otypes=[float])
     return Equation(Input, np.sum(np.exp(Input)))
-  def EquationForDerivative(Input, SumExpOfInput):
+  def EquationForDerivative(self, Input, SumExpOfInput):
     return (np.exp(Input) * (SumExpOfInput - np.exp(Input)))/(SumExpOfInput) ** 2
