1a69ac480ca64d5c33f23a3bfaaf7ea689b48023
Sean-Mabli <smdrone1@gmail.com>
2021-05-26 20:11:10 -0400

    lstm init and nn bar in terminal

--- example/LSTM/LSTM.py
+++ example/LSTM/LSTM.py
@@ -3,12 +3,29 @@ from ActivationFunctions import Sigmoid, Tanh, ReLU, LeakyReLU, StableSoftMax
 Sigmoid, Tanh, ReLU, LeakyReLU, StableSoftMax = Sigmoid(), Tanh(), ReLU(), LeakyReLU(), StableSoftMax()
 
 class LSTM:
-  def __init__(self, InputSize, OutputSize, Type, HidSize=64, LearningRate=0.05):
-    self.LearningRate, self.Type = LearningRate, Type
+  def __init__(self, InSize, OutSize, HidSize=64, LearningRate=0.05):
+    self.LearningRate = LearningRate
 
-    self.WeightsHidToHid = np.random.uniform(-0.005, 0.005, (HidSize, HidSize))
-    self.WeightsInputToHid = np.random.uniform(-0.005, 0.005, (HidSize, InputSize))
-    self.WeightsHidToOut = np.random.uniform(-0.005, 0.005, (OutputSize, HidSize))
+    self.WeightsHidToInputGate = np.random.uniform(-0.005, 0.005, (HidSize, HidSize))
+    self.WeightsHidToOutputGate = np.random.uniform(-0.005, 0.005, (HidSize, HidSize))
+    self.WeightsHidToCellMemGate = np.random.uniform(-0.005, 0.005, (HidSize, HidSize))
+    self.WeightsHidToForgetGate = np.random.uniform(-0.005, 0.005, (HidSize, HidSize))
 
-    self.HiddenBiases = np.zeros(HidSize)
-    self.OutputBiases = np.zeros(OutputSize)
+    self.WeightsInToInGate = np.random.uniform(-0.005, 0.005, (HidSize, InSize))
+    self.WeightsInToOutGate = np.random.uniform(-0.005, 0.005, (HidSize, InSize))
+    self.WeightsInToCellMemGate = np.random.uniform(-0.005, 0.005, (HidSize, InSize))
+    self.WeightsInToForgetGate = np.random.uniform(-0.005, 0.005, (HidSize, InSize))
+
+    self.InGateBiases = np.zeros(HidSize)
+    self.OuGateBiases = np.zeros(HidSize)
+    self.CellMemGateBiases = np.zeros(HidSize)
+    self.ForgetGateBiases = np.zeros(HidSize)
+
+    self.WeightsHidToOut = np.random.uniform(-0.005, 0.005, (OutSize, HidSize))
+    self.OutBias = np.zeros(OutSize)
+  
+  def ForwardProp(self, Input):
+    print("hi")
+
+  def BackProp(self):
+    print("hi")
--- example/NN/NN_Example.py
+++ example/NN/NN_Example.py
@@ -1,5 +1,6 @@
 import numpy as np
 from NN import NN
+from alive_progress import alive_bar
 
 # Neural Network Model
 InputToHidden1 = NN(InputSize=2, OutputSize=16, Activation='ReLU', LearningRate=0.1, DropoutRate=0)
@@ -7,36 +8,41 @@ Hidden1ToHidden2 = NN(InputSize=16, OutputSize=16, Activation='ReLU', LearningRa
 Hidden2ToOutput = NN(InputSize=16, OutputSize=2, Activation='Sigmoid', LearningRate=0.1, DropoutRate=0)
 
 # Training
-for Generation in range(100):
-  # Create Input And Real Output
-  Input = np.random.choice(([0, 1]), 2)
-  RealOutput = [1, 0] if sum(Input) == 1 else [0, 1]
-  
-  # Forward Prop
-  Hidden1 = InputToHidden1.ForwardProp(Input)
-  Hidden2 = Hidden1ToHidden2.ForwardProp(Hidden1)
-  Output = Hidden2ToOutput.ForwardProp(Hidden2)
-  
-  # Back Prop
-  OutputError = RealOutput - Output
-  Hidden2Error = Hidden2ToOutput.BackProp(OutputError)
-  print(Hidden2Error.dtype)
-  Hidden1Error = Hidden1ToHidden2.BackProp(Hidden2Error)
-  InputToHidden1.BackProp(Hidden1Error)
+NumOfTrainGen = 100
+NumOfTestGen = 100
 
-# Testing
-NumberCorrect = 0
+with alive_bar(NumOfTrainGen + NumOfTestGen) as bar:
+  for Generation in range(NumOfTrainGen):
+    # Create Input And Real Output
+    Input = np.random.choice(([0, 1]), 2)
+    RealOutput = [1, 0] if sum(Input) == 1 else [0, 1]
 
-for Generation in range(100):
-  # Create Input
-  Input = np.random.choice(([0, 1]), 2)
-  RealOutput = [1, 0] if sum(Input) == 1 else [0, 1]
-  
-  # Forward Prop
-  Hidden1 = InputToHidden1.ForwardProp(Input)
-  Hidden2 = Hidden1ToHidden2.ForwardProp(Hidden1)
-  Output = Hidden2ToOutput.ForwardProp(Hidden2)
-  
-  NumberCorrect += 1 if np.argmax(Output) == (0 if sum(Input) == 1 else 1) else 0
+    # Forward Prop
+    Hidden1 = InputToHidden1.ForwardProp(Input)
+    Hidden2 = Hidden1ToHidden2.ForwardProp(Hidden1)
+    Output = Hidden2ToOutput.ForwardProp(Hidden2)
 
-print(NumberCorrect / 100)
+    # Back Prop
+    OutputError = RealOutput - Output
+    Hidden2Error = Hidden2ToOutput.BackProp(OutputError)
+    Hidden1Error = Hidden1ToHidden2.BackProp(Hidden2Error)
+    InputToHidden1.BackProp(Hidden1Error)
+    bar()
+
+  # Testing
+  NumberCorrect = 0
+
+  for Generation in range(NumOfTestGen):
+    # Create Input
+    Input = np.random.choice(([0, 1]), 2)
+    RealOutput = [1, 0] if sum(Input) == 1 else [0, 1]
+
+    # Forward Prop
+    Hidden1 = InputToHidden1.ForwardProp(Input)
+    Hidden2 = Hidden1ToHidden2.ForwardProp(Hidden1)
+    Output = Hidden2ToOutput.ForwardProp(Hidden2)
+
+    NumberCorrect += 1 if np.argmax(Output) == (0 if sum(Input) == 1 else 1) else 0
+    bar()
+
+  print(NumberCorrect / NumOfTestGen)
