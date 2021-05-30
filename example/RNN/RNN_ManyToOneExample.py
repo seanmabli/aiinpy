216a549edbc16844cf690e5adb158433c50e6574
Sean-Mabli <smdrone1@gmail.com>
2021-05-30 14:46:02 -0400

    Num Of Run Training Changed To 15000 RNN

--- example/RNN/RNN_ManyToOneExample.py
+++ example/RNN/RNN_ManyToOneExample.py
@@ -6,7 +6,7 @@ from alive_progress import alive_bar
 TrainingDataUniqueWords = list(set([w for Sentence in TrainingData.keys() for w in Sentence.split(' ')]))
 Rnn = RNN(len(TrainingDataUniqueWords), 2, Type='ManyToOne', LearningRate=0.05)
 
-NumOfTrainGen = 100000
+NumOfTrainGen = 15000
 NumOfTestGen = len(list(TestData.items()))
 
 with alive_bar(NumOfTrainGen + NumOfTestGen) as bar:
