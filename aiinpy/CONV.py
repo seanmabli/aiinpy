4fc35a8bc1c9e3ff41533b7ad8d513f5dba2f2cf
Sean-Mabli <smdrone1@gmail.com>
2021-06-22 12:00:31 -0400

    CONV error in final code, starting gan stuff

--- aiinpy/CONV.py
+++ aiinpy/CONV.py
@@ -1,10 +1,12 @@
 import numpy as np
-from .ActivationFunctions import Sigmoid, Tanh, ReLU, LeakyReLU, StableSoftMax
+from ActivationFunctions import Sigmoid, Tanh, ReLU, LeakyReLU, StableSoftMax
 Sigmoid, Tanh, ReLU, LeakyReLU, StableSoftMax = Sigmoid(), Tanh(), ReLU(), LeakyReLU(), StableSoftMax()
+import sys
 
 class CONV:
   def __init__(self, FilterShape, LearningRate, Activation='None', Padding='None', Stride=(1, 1), DropoutRate=0):
     self.Filter = np.random.uniform(-0.25, 0.25, (FilterShape))
+    self.Bias = np.zeros(FilterShape[0])
     self.NumOfFilters = FilterShape[0]
     self.LearningRate, self.Activation, self.Padding, self.Stride, self.DropoutRate = LearningRate, Activation, Padding, Stride, DropoutRate
 
@@ -14,52 +16,65 @@ class CONV:
   def ChangeDropoutRate(self, NewRate):
     self.DropoutRate = NewRate
 
-  def ForwardProp(self, InputImage):
+  def ForwardProp(self, Input):
     if (self.Padding == 'None'):
-      if(InputImage.ndim == 2):
-        self.InputImage = np.stack(([InputImage] * self.NumOfFilters))
+      if(Input.ndim == 2):
+        self.Input = np.stack(([Input] * self.NumOfFilters))
       else:
-        self.InputImage = InputImage
+        self.Input = Input
     if (self.Padding == 'Same'):
-      if(InputImage.ndim == 2):
-        self.InputImage = np.stack(([np.pad(InputImage, int((len(self.Filter[0]) - 1) / 2), mode='constant')] * self.NumOfFilters))
+      if(Input.ndim == 2):
+        self.Input = np.stack(([np.pad(Input, int((len(self.Filter[0]) - 1) / 2), mode='constant')] * self.NumOfFilters))
       else:
-        self.InputImage = np.pad(InputImage, int((len(self.Filter[0]) - 1) / 2), mode='constant')[1 : self.NumOfFilters + 1]
+        self.Input = np.pad(Input, int((len(self.Filter[0]) - 1) / 2), mode='constant')[1 : self.NumOfFilters + 1]
     
-    self.OutputWidth = int((len(self.InputImage[0, 0]) - (len(self.Filter[0, 0]) - 1)) / self.Stride[0])
-    self.OutputHeight = int((len(self.InputImage[0]) - (len(self.Filter[0]) - 1)) / self.Stride[1])
+    self.OutputWidth = int((len(self.Input[0, 0]) - (len(self.Filter[0, 0]) - 1)) / self.Stride[0])
+    self.OutputHeight = int((len(self.Input[0]) - (len(self.Filter[0]) - 1)) / self.Stride[1])
 
-    self.OutputArray = np.zeros((self.NumOfFilters, self.OutputHeight, self.OutputWidth))
+    self.Output = np.zeros((self.NumOfFilters, self.OutputHeight, self.OutputWidth))
 
     for i in range(0, self.OutputWidth, self.Stride[0]):
       for j in range(0, self.OutputHeight, self.Stride[1]):
-        self.OutputArray[:, i, j] = np.sum(np.multiply(self.InputImage[:, i : i + 3, j : j + 3], self.Filter), axis=(1, 2))
-    
-    if self.Activation == 'Sigmoid': self.OutputArray = Sigmoid.Sigmoid(self.OutputArray)
-    if self.Activation == 'Tanh': self.OutputArray = Tanh.Tanh(self.OutputArray)
-    if self.Activation == 'ReLU': self.OutputArray = ReLU.ReLU(self.OutputArray)
-    if self.Activation == 'LeakyReLU': self.OutputArray = LeakyReLU.LeakyReLU(self.OutputArray)
-    if self.Activation == 'StableSoftMax': self.OutputArray = StableSoftMax.StableSoftMax(self.OutputArray)
-    if self.Activation == 'None': self.OutputArray = self.OutputArray
+        self.Output[:, i, j] = np.sum(np.multiply(self.Input[:, i : i + 3, j : j + 3], self.Filter), axis=(1, 2))
 
-    self.Dropout = np.random.binomial(1, self.DropoutRate, size=self.OutputArray.shape)
+    if self.Activation == 'Sigmoid': self.Output = Sigmoid.Sigmoid(self.Output)
+    if self.Activation == 'Tanh': self.Output = Tanh.Tanh(self.Output)
+    if self.Activation == 'ReLU': self.Output = ReLU.ReLU(self.Output)
+    if self.Activation == 'LeakyReLU': self.Output = LeakyReLU.LeakyReLU(self.Output)
+    if self.Activation == 'StableSoftMax': self.Output = StableSoftMax.StableSoftMax(self.Output)
+    if self.Activation == 'None': self.Output = self.Output
+
+    self.Dropout = np.random.binomial(1, self.DropoutRate, size=self.Output.shape)
     self.Dropout = np.where(self.Dropout == 0, 1, 0)
-    self.OutputArray *= self.Dropout
+    self.Output *= self.Dropout
 
-    return self.OutputArray
+    return self.Output
   
-  def BackProp(self, ConvolutionError):
+  def BackProp(self, OutputError):
     FilterGradients = np.zeros((self.NumOfFilters, 3, 3))
-    ConvolutionError *= self.Dropout
+    OutputError *= self.Dropout
     
-    if self.Activation == 'Sigmoid': Derivative = Sigmoid.Derivative(self.OutputArray)
-    if self.Activation == 'Tanh': Derivative = Tanh.Derivative(self.OutputArray)
-    if self.Activation == 'ReLU': Derivative = ReLU.Derivative(self.OutputArray)
-    if self.Activation == 'LeakyReLU': Derivative = LeakyReLU.Derivative(self.OutputArray)
-    if self.Activation == 'StableSoftMax': Derivative = StableSoftMax.Derivative(self.OutputArray)
-    if self.Activation == 'None': Derivative = self.OutputArray
+    if self.Activation == 'Sigmoid': Derivative = Sigmoid.Derivative(self.Output)
+    if self.Activation == 'Tanh': Derivative = Tanh.Derivative(self.Output)
+    if self.Activation == 'ReLU': Derivative = ReLU.Derivative(self.Output)
+    if self.Activation == 'LeakyReLU': Derivative = LeakyReLU.Derivative(self.Output)
+    if self.Activation == 'StableSoftMax': Derivative = StableSoftMax.Derivative(self.Output)
+    if self.Activation == 'None': Derivative = self.Output
+
+    x = OutputError * Derivative
 
     for i in range(0, self.OutputWidth, self.Stride[0]):
       for j in range(0, self.OutputHeight, self.Stride[1]):
-        FilterGradients += self.InputImage[:, i : (i + 3), j : (j + 3)] * ConvolutionError[:, i, j][:, np.newaxis, np.newaxis] * Derivative[:, i, j][:, np.newaxis, np.newaxis]
+        FilterGradients += self.Input[:, i : (i + 3), j : (j + 3)] * x[:, i, j][:, np.newaxis, np.newaxis]
+    
     self.Filter += FilterGradients * self.LearningRate
+
+    self.FilterFliped = np.rot90(np.rot90(self.Filter))
+    y = np.pad(OutputError, 2, mode='constant')[2 : self.NumOfFilters + 2, :, :]
+
+    self.PreviousError = np.zeros(self.Input.shape)
+    for i in range(self.OutputWidth + len(self.Filter[0, 0]) - 1):
+      for j in range(self.OutputHeight + len(self.Filter[0]) - 1):
+        self.PreviousError[:, i, j] = np.sum(np.multiply(self.FilterFliped, y[:, i:i+3, j:j+3]), axis=(1, 2))
+
+    return self.PreviousError
