056c45da4746cd63074c56676adf912eefb5dc69
Sean-Mabli <smdrone1@gmail.com>
2021-10-03 19:36:22 -0400

    Fix 2nd MatMul And Add Mask (Opt.) To Transformer

--- Transformer/Transformer.py
+++ Transformer/Transformer.py
@@ -17,31 +17,39 @@ def SingleHeadSelfAttention(In):
     Value[i, :] = InToValue.ForwardProp(In[i, :])
 
   Out = np.dot(In, np.transpose(In))
-  Out /= In.shape[1] ** 0.5
-  # Didn't include Mask (opt.)
-
+  Out = Out / In.shape[1] ** 0.5
+  
+  Out[np.triu_indices(Out.shape[0], 1)] = float('-inf')
+  
   Out = ApplyActivation(Out, "StableSoftmax")
-  Out = np.sum(Out)
-  Out *= Value
+  Out = Out @ Value
 
   return Out
 
 def MultiHeadSelfAttention(Input, NumOfHeads):
-  InToKey = NN(InputSize=Input.shape[1], OutSize=(Input.shape[1] * NumOfHeads), Activation='Identity', LearningRate=0)
-  InToQuery = NN(InputSize=Input.shape[1], OutSize=(Input.shape[1] * NumOfHeads), Activation='Identity', LearningRate=0)
-  InToValue = NN(InputSize=Input.shape[1], OutSize=(Input.shape[1] * NumOfHeads), Activation='Identity', LearningRate=0)
+  InToKey = NN(InSize=Input.shape[1], OutSize=(Input.shape[1] * NumOfHeads), Activation='Identity', LearningRate=0)
+  InToQuery = NN(InSize=Input.shape[1], OutSize=(Input.shape[1] * NumOfHeads), Activation='Identity', LearningRate=0)
+  InToValue = NN(InSize=Input.shape[1], OutSize=(Input.shape[1] * NumOfHeads), Activation='Identity', LearningRate=0)
+  KeyQueryValueToOut = NN(InSize=(Input.shape[1] * NumOfHeads), OutSize=Input.shape[1], Activation='Identity', LearningRate=0)
+
   Key = np.zeros((Input.shape[0], Input.shape[1] * NumOfHeads))
   Query = np.zeros((Input.shape[0], Input.shape[1] * NumOfHeads))
   Value = np.zeros((Input.shape[0], Input.shape[1] * NumOfHeads))
+
   for i in range(Input.shape[0]):
     Key[i, :] = InToKey.ForwardProp(Input[i, :])
     Query[i, :] = InToQuery.ForwardProp(Input[i, :])
     Value[i, :] = InToValue.ForwardProp(Input[i, :])
   
-  Weights = np.dot(Key, np.transpose(Query))
+  Out = np.dot(Query, np.transpose(Key))
 
-  Weights = ApplyActivation(Weights, "StableSoftmax")
-  print(Weights)
+  Out = ApplyActivation(Out, "StableSoftmax")
+  Out = np.sum(Out)
+  Out *= Value
+
+  Out = KeyQueryValueToOut.ForwardProp(Out)
+  return Out
+
 
 Input = np.array([[1, 0, 0], [0, 0, 1]])
 print(SingleHeadSelfAttention(Input))
