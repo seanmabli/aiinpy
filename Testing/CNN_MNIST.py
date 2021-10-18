d90a3c6cf85b3f7769b36b560bf265cee07590db
Sean-Mabli <smdrone1@gmail.com>
2021-10-18 16:35:30 -0400

    Add Min, Mean, Sum Pooling

--- Testing/CNN_MNIST.py
+++ Testing/CNN_MNIST.py
@@ -4,16 +4,15 @@ from TestSrc.NN import NN
 from TestSrc.CONV import CONV
 from TestSrc.POOL import POOL
 from alive_progress import alive_bar
-import sys
 import wandb
 
-wandb.init(project='cnn')
+wandb.init(project='pool_test')
 
 InToConv1 = CONV((4, 3, 3), LearningRate=0.01, Padding=True, Activation='ReLU')
-Conv1ToPool1 = POOL((2, 2))
-DenseInToOut = NN(InSize=(4 * 14 * 14), OutSize=10, Activation='StableSoftmax', LearningRate=0.1, WeightsInit=(0, 0))
+Conv1ToPool1 = POOL((2, 2), (2, 2), 'Sum')
+DenseInToOut = NN(InShape=(4 * 14 * 14), OutShape=10, Activation='StableSoftmax', LearningRate=0.1, WeightsInit=(0, 0))
 
-# Load EMNIST Training An1d Testing Images
+# Load EMNIST Training And Testing Images
 TestImageLoaded = 1000
 TrainingImages, TrainingLabels = extract_training_samples('digits')
 TestImages, TestLabels = extract_test_samples('digits')[0 : TestImageLoaded]
@@ -42,18 +41,7 @@ with alive_bar(NumOfTrainGen + TestImageLoaded) as bar:
     Conv1Error = Conv1ToPool1.BackProp(Pool1Error)
     InError = InToConv1.BackProp(Conv1Error)
     
-    wandb.log({"In": np.sum(abs(In)) / In.size,
-               "Conv1": np.sum(abs(Conv1)) / Conv1.size,
-               "Pool1": np.sum(abs(Pool1)) / Pool1.size,
-               "DenseIn": np.sum(abs(DenseIn)) / DenseIn.size, 
-               "Out": np.sum(abs(Out)) / Out.size, 
-
-               "OutError": np.sum(abs(OutError)) / OutError.size, 
-               "DenseInError": np.sum(abs(DenseInError)) / DenseInError.size, 
-               "Pool1Error": np.sum(abs(Pool1Error)) / Pool1Error.size,
-               "Conv1Error": np.sum(abs(Conv1Error)) / Conv1Error.size,
-               "InError": np.sum(abs(InError)) / InError.size,
-               "Correct": 1 if np.argmax(Out) == TrainingLabels[Random] else 0})
+    wandb.log({"Train Correct": 1 if np.argmax(Out) == TrainingLabels[Random] else 0})
 
     bar()
   
@@ -68,8 +56,7 @@ with alive_bar(NumOfTrainGen + TestImageLoaded) as bar:
     
     NumberCorrect += 1 if np.argmax(Out) == TestLabels[Generation] else 0
     
-    wandb.log({"Test Correct": 1 if np.argmax(Out) == TestLabels[Generation] else 0})
-
     bar()
 
+  wandb.log({"Test Correct": NumberCorrect / TestImageLoaded})
   print(NumberCorrect / TestImageLoaded)
--- Testing/TestSrc/POOL.py
+++ Testing/TestSrc/POOL.py
@@ -1,18 +1,32 @@
 import numpy as np
 
 class POOL: 
-  def __init__(self, Stride, Type):
-    self.Stride = Stride
+  def __init__(self, Stride, FilterShape, Type):
+    self.Stride, self.FilterShape, self.Type = Stride, FilterShape, Type
     
   def ForwardProp(self, In):
-    self.In, InShape = In, In.shape
-    OutputWidth = int(len(In[0, 0]) / 2)
-    OutputHeight = int(len(In[0]) / 2)
-    self.Out = np.zeros((len(In), OutputHeight, OutputWidth))
-    for i in range(OutputHeight):
-      for j in range(OutputWidth):
-        self.Out[:, i, j] = np.amax(In[:, i * self.Stride[0] : i * self.Stride[0] + 2, j * self.Stride[1] : j * self.Stride[1] + 2], axis=(1, 2))
+    self.In = In
+    self.Out = np.zeros((len(In), int(len(In[0]) / 2), int(len(In[0, 0]) / 2)))
+    if self.Type == 'Max':
+      for i in range(self.Out.shape[1]):
+        for j in range(self.Out.shape[2]):
+          self.Out[:, i, j] = np.amax(In[:, i * self.Stride[0] : i * self.Stride[0] + 2, j * self.Stride[1] : j * self.Stride[1] + 2], axis=(1, 2))
+    elif self.Type == 'Min':
+      for i in range(self.Out.shape[1]):
+        for j in range(self.Out.shape[2]):
+          self.Out[:, i, j] = np.amin(In[:, i * self.Stride[0] : i * self.Stride[0] + 2, j * self.Stride[1] : j * self.Stride[1] + 2], axis=(1, 2))
+    elif self.Type == 'Mean':
+      for i in range(self.Out.shape[1]):
+        for j in range(self.Out.shape[2]):
+          self.Out[:, i, j] = np.mean(In[:, i * self.Stride[0] : i * self.Stride[0] + 2, j * self.Stride[1] : j * self.Stride[1] + 2], axis=(1, 2))
+    elif self.Type == 'Sum':
+      for i in range(self.Out.shape[1]):
+        for j in range(self.Out.shape[2]):
+          self.Out[:, i, j] = np.sum(In[:, i * self.Stride[0] : i * self.Stride[0] + 2, j * self.Stride[1] : j * self.Stride[1] + 2], axis=(1, 2))
     return self.Out
 
   def BackProp(self, OutError):
-    return np.repeat(np.repeat(OutError, 2, axis=1), 2, axis=2) * np.equal(np.repeat(np.repeat(self.Out, 2, axis=1), 2, axis=2), self.In).astype(int)
+    if self.Type == 'Max' or self.Type == 'Min':
+      return np.repeat(np.repeat(OutError, 2, axis=1), 2, axis=2) * np.equal(np.repeat(np.repeat(self.Out, 2, axis=1), 2, axis=2), self.In).astype(int)
+    elif self.Type == 'Mean' or self.Type == 'Sum':
+      return np.repeat(np.repeat(OutError, 2, axis=1), 2, axis=2) * np.repeat(np.repeat(self.Out, 2, axis=1), 2, axis=2)
