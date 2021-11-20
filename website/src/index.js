720e7a24f6a2a8719a7a9a079704d9fbdbbf0d6f
SeanMabli <smdrone1@gmail.com>
2021-11-20 08:07:23 -0500

    work on website

--- website/src/index.js
+++ website/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import blog from './blog';
+import Post from './post';
 import './index.css';
 
 ReactDOM.render(
-  <p>hello world</p>,
+    <Router>
+      <Switch>
+        <Route exact path="/" component={blog} />
+
+        <Route path="/:id" component={Post} />
+      </Switch>
+    </Router>,
 
   document.getElementById('root'));
---
+++ website/src/post.js
@@ -0,0 +1,27 @@
+import { useParams } from "react-router";
+import content from './content';
+import './index.css';
+
+
+function Post() {
+
+  const { id } = useParams();
+  const postcontent = content.filter(content => content.id === id);
+
+  return (
+    <div className="site">
+      {postcontent.map((item) => {
+        return (
+          <div className="box">
+            <p>{item.title}</p>
+            <h1 className="lighter">{item.published}</h1>
+            <h1>&nbsp;{item.tag}</h1>
+            <h2>{item.artical}</h2>
+          </div>
+        ) 
+      })}
+    </div>
+  );
+}
+
+export default Post;
