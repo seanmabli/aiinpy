1bff7aeb54882d684e7e3950b31667c0b402fe53
SeanMabli <smdrone1@gmail.com>
2021-11-22 20:26:34 -0500

    integrate json with post.js

--- testing/websiteinfo.py
+++ testing/websiteinfo.py
@@ -7,6 +7,7 @@ classes = np.array([], dtype=object)
 title = np.array([])
 source = np.array([])
 url = np.array([])
+urlid = np.array([])
 
 for name, obj in inspect.getmembers(ai):
   if inspect.isclass(obj):
@@ -14,15 +15,18 @@ for name, obj in inspect.getmembers(ai):
 
 for i in range(len(classes)):
   title = np.append(title, 'aiinpy.' + classes[i].__name__)
-  url = np.append(url, classes[i].__name__)
+  url = np.append(url, '/' + classes[i].__name__)
+  urlid = np.append(urlid, classes[i].__name__ )
   source = np.append(source, inspect.getsource(classes[i]))
   if source[i].find('__init__') != -1:
     source[i] = 'aiinpy' + classes[i].__name__ + source[i][source[i].find('__init__') + 8 : source[i].find('):') + 1]
   else:
     source[i] = 'aiinpy' + classes[i].__name__ + '()'
 
-data = np.array([title, source, url], dtype=str)
-data = np.rot90(data)
+data = np.array([title, source, url, urlid], dtype=str)
+data = np.rot90(data).tolist()
+
+jsondata = [{"title" : data[i][0], "function" : data[i][1], "url" : data[i][2], "id" : data[i][3]} for i in range(len(data))]
 
 with open("website\src\content.json", "w") as write_file:
-  json.dump({"pages": data.tolist()}, write_file, sort_keys=True, indent=2)
+  json.dump(jsondata, write_file, indent=2)
--- website/src/post.js
+++ website/src/post.js
@@ -1,33 +1,35 @@
 import { useParams } from "react-router";
 import { Link } from 'react-router-dom';
-import pages from './content.json';
+import content from './content.json';
 import './index.css';
-
 
 function Post() {
 
   const { id } = useParams();
-  const postcontent = pages.filter(pages => pages.url === id);
+  console.log(id);
+  const postcontent = content.filter(content => content.id === id);
 
   return (
     <div className="site">
       <div className="about">
         <Link to="/" className="p notext-decoration">aiinpy</Link>
-        {pages.map((item) => {
+        {content.map((item) => {
           return (
             <div>
               <Link to={item.url} className="h2 link"> {item.title} </Link> <br />
             </div>
+          )
+        })}
+      </div>
+      <div className="blog">
+        {postcontent.map((item) => {
+          return (
+            <p>{item.title}</p>
           ) 
         })}
       </div>
     </div>
   );
 }
-/*
-      {postcontent.map((item) => {
-        return (
-        ) 
-      })}
-      */
+
 export default Post;
