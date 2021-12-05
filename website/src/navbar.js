69c12a45cf025b2e32af9dfeb79d22f58d932195
SeanMabli <smdrone1@gmail.com>
2021-12-05 14:36:53 -0500

    add bottom links to website

--- website/src/navbar.js
+++ website/src/navbar.js
@@ -29,6 +29,10 @@ function Navbar() {
           </div>
         )
       })}
+      <div className="box">
+        <a href="https://github.com/seanmabli/aiinpy" className="h1 lighter link">github</a> 
+        <a href="https://pypi.org/project/aiinpy/" className="h1 lighter link">&nbsp; pypi</a> 
+      </div>
     </div>
   );
 }
