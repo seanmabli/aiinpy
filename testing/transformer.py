e94db56a9966ed4c9b8d7c68dc50c2cb9a0e085c
SeanMabli <smdrone1@gmail.com>
2021-12-05 13:48:18 -0500

    work on transformer

--- testing/transformer.py
+++ testing/transformer.py
@@ -1,44 +1,53 @@
 import testsrc as ai
+import numpy as np
 
-def SingleHeadSelfattention(in):
-  inToKey = NN(inshape=in.shape, outshape=in.shape, activation='Identity', learningrate=0)
-  inToQuery = NN(inshape=in.shape, outshape=in.shape, activation='Identity', learningrate=0)
-  inToValue = NN(inshape=in.shape, outshape=in.shape, activation='Identity', learningrate=0)
+def basicselfattention(input):
+  weights = np.dot(input, input.T)
+  weights = ai.stablesoftmax().forward(weights)
+  out = weights @ input
+  return out
 
-  Key = inToKey.forward(in)
-  Query = inToQuery.forward(in)
-  Value = inToValue.forward(in)
+def singleheadselfattention(input):
+  inToKey = ai.nn(inshape=input.shape, outshape=input.shape, activation=ai.identity, learningrate=0)
+  inToQuery = ai.nn(inshape=input.shape, outshape=input.shape, activation=ai.identity, learningrate=0)
+  inToValue = ai.nn(inshape=input.shape, outshape=input.shape, activation=ai.identity, learningrate=0)
+
+  Key = inToKey.forward(input)
+  Query = inToQuery.forward(input)
+  Value = inToValue.forward(input)
 
   out = np.dot(Query, Key.T) # MatMul
-  out = out / in.shape[1] ** 0.5 # Scale
+  out = out / input.shape[1] ** 0.5 # Scale
   
   out[np.triu_indices(out.shape[0], 1)] = float('-inf') # Mask (opt.)
 
-  out = applyactivation(out, "StableSoftmax") # SoftMax
+  out = ai.stablesoftmax().forward(out) # SoftMax
   out = out @ Value # MatMul
 
   return out
 
-def MultiHeadSelfattention(in, NumOfHeads):
-  inToKey = NN(inshape=in.shape, outshape=(in.shape[0], in.shape[1] * NumOfHeads), activation='Identity', learningrate=0)
-  inToQuery = NN(inshape=in.shape, outshape=(in.shape[0], in.shape[1] * NumOfHeads), activation='Identity', learningrate=0)
-  inToValue = NN(inshape=in.shape, outshape=(in.shape[0], in.shape[1] * NumOfHeads), activation='Identity', learningrate=0)
-  KeyQueryValueToout = NN(inshape=(in.shape[0], in.shape[1] * NumOfHeads), outshape=in.shape, activation='Identity', learningrate=0)
+def multiheadselfattention(input, NumOfHeads):
+  inToKey = ai.nn(inshape=input.shape, outshape=(input.shape[0], input.shape[1] * NumOfHeads), activation=ai.identity, learningrate=0)
+  inToQuery = ai.nn(inshape=input.shape, outshape=(input.shape[0], input.shape[1] * NumOfHeads), activation=ai.identity, learningrate=0)
+  inToValue = ai.nn(inshape=input.shape, outshape=(input.shape[0], input.shape[1] * NumOfHeads), activation=ai.identity, learningrate=0)
+  KeyQueryValueToout = ai.nn(inshape=(input.shape[0], input.shape[1] * NumOfHeads), outshape=input.shape, activation=ai.identity, learningrate=0)
 
-  Key = inToKey.forward(in)
-  Query = inToQuery.forward(in)
-  Value = inToValue.forward(in)
+  Key = inToKey.forward(input)
+  Query = inToQuery.forward(input)
+  Value = inToValue.forward(input)
 
   out = np.dot(Query, Key.T) # MatMul
-  out = out / in.shape[1] ** 0.5 # Scale
+  out = out / input.shape[1] ** 0.5 # Scale
   
   out[np.triu_indices(out.shape[0], 1)] = float('-inf') # Mask (opt.)
 
-  out = applyactivation(out, "StableSoftmax") # SoftMax
+  out = ai.stablesoftmax().forward(out) # SoftMax
   out = out @ Value # MatMul
 
   out = KeyQueryValueToout.forward(out)
   return out
 
 input = np.array([[1, 0, 0], [0, 0, 1]])
-print(MultiHeadSelfattention(input, 8))
+print(basicselfattention(input))
+print(singleheadselfattention(input))
+print(multiheadselfattention(input, 8))
