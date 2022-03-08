1576790e7b23a38dade32bd1ff398d260cf5c3e6
sean mabli <70709738+seanmabli@users.noreply.github.com>
2022-03-08 17:08:24 +0000

    remove all nonmain stuff

--- testing/activation-testinput.py
+++
@@ -1,9 +0,0 @@
-import testsrc as ai
-import numpy as np
-
-prelu = ai.prelu(0.01)
-
-input = np.array([-12673, -36, -1, -0.7, 0, 0.7, 1, 36, 12673])
-output = prelu.forward(input)
-
-print(np.round(output))
--- testing/testsrc/softplus.py
+++
@@ -1,6 +0,0 @@
-class softplus:
-  def forward(self, input):
-    return np.log(1 + np.exp(input))
-    
-  def backward(self, input):
-    return 1 / (1 + np.exp(-input))
