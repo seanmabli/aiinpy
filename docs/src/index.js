2123501251868c520a69ce06cec12ee3dc166134
sean mabli <smdrone1@gmail.com>
2022-03-18 15:23:55 +0000

    update website to new freact-router-dom version

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './about';
 import Function from './function';
 import './index.css';
@@ -7,13 +7,12 @@ import './index.css';
 window.currentversion = '0.0.17';
 window.versions = ["0.0.17", "0.0.16", "0.0.13", "0.0.11"];
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route exact path="/" element={About} />
-      <Route exact path="/:version" element={About} />
-      <Route exact path="/:version/:func" element={Function} />
-    </Routes>
-  </Router>,
-
+render(
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<About/>} />
+        <Route exact path="/:version" element={<About/>} />
+        <Route exact path="/:version/:func" element={<Function/>} />
+      </Routes>
+    </BrowserRouter>,
 document.getElementById('root'));
