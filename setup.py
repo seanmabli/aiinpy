9d7b03ca0f9ed7a2b7402f095583dac04b500692
sean mabli <smdrone1@gmail.com>
2022-04-01 18:13:31 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -1,10 +1,2 @@
 from setuptools
 setuptools.setup()
-
-setup(name='aiinpy',
-      version='0.0.17',
-      description='An artificial intelligence library?',
-      license='MIT',
-      packages=['aiinpy'],
-      url='https://aiinpy.org',
-      install_requires=['numpy'])
