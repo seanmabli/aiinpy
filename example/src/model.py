work on activation tsting, update nn with correct activation derivative (i think), fix sigmiod derivative, fix tanh derivative and improve effecientcy, make mish derivative standalone

example/src/model.py
@@ -1,14 +1,19 @@
 import numpy as np
 import sys, os, time, json, random, datetime
+import wandb
 
 class model:
-  def __init__(self, inshape, outshape, model):
+  def __init__(self, inshape, outshape, model, wandbproject=None):
     self.inshape = inshape = inshape if isinstance(inshape, tuple) else tuple([inshape])
     self.outshape = outshape if isinstance(outshape, tuple) else tuple([outshape])
     self.model = model
+    self.wandbproject = wandbproject
 
     for i in self.model:
       inshape = i.modelinit(inshape)
+    
+    if wandbproject is not None:
+      wandb.init(project=wandbproject)
 
   def forward(self, input):
     for i in range(len(self.model)):
@@ -36,17 +41,31 @@ class model:
       x.pop(data[1].shape.index(NumOfData))
       data[1] = np.transpose(data[1], tuple([data[1].shape.index(NumOfData)]) + tuple(x))
 
-    # Training
-    for gen in range(numofgen):
-      random = np.random.randint(0, NumOfData)
-      input = data[0][random]
-      for i in range(len(self.model)):
-        input = self.model[i].forward(input)
+    # Training, with wandb
+    if self.wandbproject is not None:
+      for gen in range(numofgen):
+        random = np.random.randint(0, NumOfData)
+        input = data[0][random]
+        for i in range(len(self.model)):
+          input = self.model[i].forward(input)
 
-      outerror = data[1][random] - input
-      for i in reversed(range(len(self.model))):
-        outerror = self.model[i].backward(outerror)
-      sys.stdout.write('\r' + 'training: ' + str(gen + 1) + '/' + str(numofgen))
+        outerror = data[1][random] - input
+        wandb.log({'train error': np.sum(abs(outerror))})
+        for i in reversed(range(len(self.model))):
+          outerror = self.model[i].backward(outerror)
+        # sys.stdout.write('\r' + 'training: ' + str(gen + 1) + '/' + str(numofgen))
+    else:
+      # Training, without wandb
+      for gen in range(numofgen):
+        random = np.random.randint(0, NumOfData)
+        input = data[0][random]
+        for i in range(len(self.model)):
+          input = self.model[i].forward(input)
+
+        outerror = data[1][random] - input
+        for i in reversed(range(len(self.model))):
+          outerror = self.model[i].backward(outerror)
+        # sys.stdout.write('\r' + 'training: ' + str(gen + 1) + '/' + str(numofgen))
     
     print('')
 
@@ -74,11 +93,13 @@ class model:
         input = self.model[i].forward(input)
 
       testcorrect += 1 if np.argmax(input) == np.argmax(data[1][gen]) else 0
-      sys.stdout.write('\r' + 'testing: ' + str(gen + 1) + '/' + str(NumOfData))
+      # sys.stdout.write('\r' + 'testing: ' + str(gen + 1) + '/' + str(NumOfData))
 
     print('')
     
     self.testaccuracy = testcorrect / NumOfData
+    if self.wandbproject is not None:
+      wandb.log({'test accuracy': self.testaccuracy})
     return self.testaccuracy
 
   def use(self, indata):

example/src/nn.py
@@ -36,11 +36,12 @@ class nn:
   def forward(self, input):
     self.input = input.flatten()
     self.out = self.activation.forward(self.weights.T @ self.input + self.biases)
+    self.derivative = self.activation.backward(self.weights.T @ self.input + self.biases) # now it applys the derivative to the output without the activation function, check if this is right
     return self.out.reshape(self.outshape)
 
   def backward(self, outerror):
     outerror = outerror.flatten()
-    outgradient = self.activation.backward(self.out) * outerror
+    outgradient = self.derivative * outerror
     inputerror = self.weights @ outerror
     self.biases += outgradient * self.learningrate
     self.weights += np.outer(self.input.T, outgradient) * self.learningrate