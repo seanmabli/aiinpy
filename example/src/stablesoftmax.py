work on correct stable softmax derivative

example/src/stablesoftmax.py
@@ -8,8 +8,15 @@ class stablesoftmax:
     return np.exp(input - np.max(input)) / np.sum(np.exp(input - np.max(input)))
 
   def backward(self, input):
-    equation = np.vectorize(self.equationforderivative, otypes=[float])
-    return equation(input, np.sum(np.exp(input)))
-    
-  def equationforderivative(self, input, SumExpOfinput):
-    return (np.exp(input) * (SumExpOfinput - np.exp(input))) / (SumExpOfinput) ** 2
+    forward = np.exp(input - np.max(input)) / np.sum(np.exp(input - np.max(input)))
+    out = np.zeros((len(input), len(input)))
+    for i in range(len(input)):
+      for j in range(len(input)):
+        out[i, j] = input[i] * (1 - input[j]) if i == j else - input[i] * input[j]
+    return np.sum(out, axis=0)
+
+x = [1, 5, 7]
+z = stablesoftmax().forward(x)
+y = stablesoftmax().backward(x)
+print(np.round(z, 3))
+print(np.round(y, 3))
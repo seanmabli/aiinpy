cc93973f9ea73cbaa2d5e77c461e81dd07d8a22e
sean mabli <smdrone1@gmail.com>
2022-05-11 00:45:54 +0000

    work on correct stable softmax derivative

--- example/cnn-mnist.py
+++ example/cnn-mnist.py
@@ -19,7 +19,7 @@ model = ai.model((28, 28), 10, [
   ai.conv(inshape=(28, 28), filtershape=(4, 3, 3), learningrate=0.01, activation=ai.relu()),
   ai.pool(stride=(2, 2), filtershape=(2, 2), opperation='Max'),
   ai.nn(outshape=10, activation=ai.stablesoftmax(), learningrate=0.1, weightsinit=(0, 0))
-], wandbproject='cnn-mnist', usebestcache=True)
+], wandbproject='cnn-mnist')
 
 model.train((inTrainData, outTrainDataReal), 5000)
 model.test((inTestData, outTestDataReal))
--- example/src/stablesoftmax.py
+++ example/src/stablesoftmax.py
@@ -8,8 +8,15 @@ class stablesoftmax:
     return np.exp(input - np.max(input)) / np.sum(np.exp(input - np.max(input)))
 
   def backward(self, input):
-    equation = np.vectorize(self.equationforderivative, otypes=[float])
-    return equation(input, np.sum(np.exp(input)))
-    
-  def equationforderivative(self, input, SumExpOfinput):
-    return (np.exp(input) * (SumExpOfinput - np.exp(input))) / (SumExpOfinput) ** 2
+    forward = np.exp(input - np.max(input)) / np.sum(np.exp(input - np.max(input)))
+    out = np.zeros((len(input), len(input)))
+    for i in range(len(input)):
+      for j in range(len(input)):
+        out[i, j] = input[i] * (1 - input[j]) if i == j else - input[i] * input[j]
+    return np.sum(out, axis=0)
+
+x = [1, 5, 7]
+z = stablesoftmax().forward(x)
+y = stablesoftmax().backward(x)
+print(np.round(z, 3))
+print(np.round(y, 3))
