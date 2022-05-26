fix derivative problem w/ convmatrix

example/src/conv.py
@@ -62,7 +62,7 @@ class conv:
         self.out[:, i, j] = np.sum(np.multiply(self.input[:, i : i + self.filtershape[1], j : j + self.filtershape[2]], self.filter), axis=(1, 2))
 
     self.out += self.bias[:, np.newaxis, np.newaxis]
-    self.derivative = self.activation.backward(self.out) # now it applys the derivative to the output without the activation function
+    self.derivative = self.activation.backward(self.out)
     self.out = self.activation.forward(self.out)
     return self.out
   

example/src/convmatrix.py
@@ -44,13 +44,15 @@ class convmatrix:
 
   def forward(self, input):
     self.input = input.flatten()
-    self.out = self.activation.forward(self.input @ self.filtermatrix)
+    out = self.input @ self.filtermatrix
+    self.out = self.activation.forward(out)
+    self.derivative = self.activation.backward(out)
 
     return self.out.reshape(self.outshape)
 
   def backward(self, outerror):
     outerror = outerror.flatten()
-    outgradient = self.activation.backward(self.out) * outerror
+    outgradient = self.derivative * outerror
     # inputerror = np.flip((self.filtermatrix @ outerror).reshape(self.inshape), axis=1)
     self.filtermatrix += np.outer(self.input.T, outgradient) * self.learningrate
     # return inputerror