08ea5b1f4706a1d835e263f28bd907157bf5ac74
SeanMabli <smdrone1@gmail.com>
2022-07-04 00:00:07 -0400

    convert nnexample to c

--- aiinpyc/array.c
+++ aiinpyc/array.c
@@ -3,3 +3,14 @@ typedef struct
   unsigned int *shape;
   double *data;
 } array;
+
+int arraysum(array *a)
+{
+  int sum = 0;
+  printf("%ld\n", sizeof(a->shape));
+  for (int i = 0; i < a->shape[0]; i++)
+  {
+    sum += a->data[i];
+  }
+  return sum;
+}
