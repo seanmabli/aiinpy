efd83fe0d94b7b67df08f9898fcadb94a7b04ef1
sean mabli <70709738+seanmabli@users.noreply.github.com>
2022-12-20 01:08:06 +0000

    add mish to cpp activation functions

--- aiinpycpp/activation-test.cpp
+++ aiinpycpp/activation-test.cpp
@@ -11,20 +11,21 @@
 // #include "relu.cpp"
 // #include "selu.cpp"
 // #include "leakyrelu.cpp"
+#include "mish.cpp"
 
 using namespace std;
 
 int main() {
   double input[] = { -1.5483257342, 3.54324, -4.54832905, 5.87629 };
 
-  // binarystep activation;
+  mish activation;
 
   transform(begin(input), end(input), begin(input), [](double input){ return input * input; });
 
   printarraydouble("forward: ", input);
 
-  // printvectordouble("forward: ", activation.forward(input));
-  // printvectordouble("backward: ", activation.backward(input));  
+  printvectordouble("forward: ", activation.forward(input));
+  printvectordouble("backward: ", activation.backward(input));  
 
   return 0;
 }
