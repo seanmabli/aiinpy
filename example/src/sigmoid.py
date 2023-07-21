54f66d52940a1cbc404df3d4ab274e8b0321cdbc
Sean Mabli <smdrone1@gmail.com>
2023-07-20 21:35:34 -0400

    work on an autograd

--- example/src/sigmoid.py
+++ example/src/sigmoid.py
@@ -1,12 +1,11 @@
-import numpy as np
+from .tensor import tensor
 
 class sigmoid:
   def __repr__(self):
     return 'sigmoid()'
 
   def forward(self, input):
-    return 1 / (1 + np.exp(-input))
+    return 1 / (1 + tensor.exp(-input))
     
   def backward(self, input):
-    # return input * (1 - input) # this is when the input is already passed through the sigmoid function, the real derivative is below
-    return np.exp(-input) / np.square(1 + np.exp(-input))
+    return tensor.exp(-input) / ((1 + tensor.exp(-input)) ** 2)
--- example/src/tensor.py
+++ example/src/tensor.py
@@ -1,22 +1,34 @@
 import numpy as np
 
 class tensor:
-	def __init__(self, data, dtype=None):
+	def __init__(self, data, ops=[], dtype=None, marker=None):
 		self.data = (data if dtype == None else data.astype(dtype)) if type(data) == np.ndarray else np.array(data, dtype=dtype)
 		self.dtype = self.data.dtype if dtype == None else dtype
-		self.ops = [('init', self.data, self.dtype)]
+		self.ops = ['init', [], self.data] if ops == [] else ops
 		self.shape = self.data.shape
 
-	def __add__(self, other): other = self.ops.append(('add', other)); return tensor(self.data + other.data) if type(other) == tensor else tensor(self.data + other)
-	def __mul__(self, other): other = self.ops.append(('mul', other)); return tensor(self.data * other.data) if type(other) == tensor else tensor(self.data * other)
-	def __sub__(self, other): other = self.ops.append(('add', -other)); return tensor(self.data - other.data) if type(other) == tensor else tensor(self.data - other)
-	def __truediv__(self, other): other = self.ops.append(('mul', 1 / other)); return tensor(self.data / other.data) if type(other) == tensor else tensor(self.data / other)
-	def __pow__(self, other): other = self.ops.append(('pow', other)); return tensor(self.data ** other.data) if type(other) == tensor else tensor(self.data ** other)
-	def __mod__(self, other): other = self.ops.append(('mod', other)); return tensor(self.data % other.data) if type(other) == tensor else tensor(self.data % other)
-	def __matmul__(self, other): other = self.ops.append(('matmul', other)); return tensor(self.data @ other.data) if type(other) == tensor else tensor(self.data @ other)
-	def __neg__(self): other = self.ops.append(('mul', tensor(-1))); return tensor(-self.data)
-	def reshape(self, shape): other = self.ops.append(('reshape', shape)); return tensor(self.data.reshape(shape))
-	def marker(self, marker:str): other = self.ops.append(('marker', marker)); return self
+	def toTensor(self, other): return tensor(other) if type(other) != tensor else other
+
+	def __add__(self, other): other = self.toTensor(other); return tensor(self.data + other.data, ops=['add', self.ops, other.ops, self.data + other.data])
+	def __mul__(self, other): other = self.toTensor(other); return tensor(self.data * other.data, ops=['mul', self.ops, other.ops, self.data * other.data])
+	def __sub__(self, other): other = self.toTensor(other); return tensor(self.data - other.data, ops=['add', self.ops, ['mul', other.ops, ['init', [], tensor(-1)]], self.data - other.data])
+	def __truediv__(self, other): other = self.toTensor(other); return tensor(self.data / other.data, ops=['mul', self.ops, ['pow', other.ops, ['init', [], tensor(-1)]], self.data / other.data])
+	def __pow__(self, other): other = self.toTensor(other); return tensor(self.data ** other.data, ops=['pow', self.ops, other.ops, self.data ** other.data])
+	def __mod__(self, other): other = self.toTensor(other); return tensor(self.data % other.data, ops=['mod', self.ops, other.ops, self.data % other.data])
+	def __matmul__(self, other): other = self.toTensor(other); return tensor(self.data @ other.data, ops=['matmul', self.ops, other.ops, self.data @ other.data])
+	def __neg__(self): return tensor(-self.data, ops=['mul', self.ops, ['init', tensor(-1), self.dtype], -self.data])
+	def __abs__(self): return tensor(abs(self.data), ops=['abs', self.ops, abs(self.data)])
+
+	def __radd__(self, other): return self + other
+	def __rmul__(self, other): return self * other
+	def __rsub__(self, other): return self - other
+	def __rtruediv__(self, other): return self / other
+	def __rpow__(self, other): return self ** other
+	def __rmod__(self, other): return self % other
+	def __rmatmul__(self, other): return self @ other
+
+	def reshape(self, shape, other=None): return tensor(other.data.reshape(shape), ops=['reshape', other.ops, shape]) if other != None else tensor(self.data.reshape(shape), ops=['reshape', self.ops, shape])
+	def exp(self, other=None): return tensor(np.exp(other.data), ops=['pow', other.ops, ['init', tensor(np.e), other.dtype]]) if other != None else tensor(np.exp(self.data), ops=['pow', self.ops, ['init', tensor(np.e), self.dtype]])
 
 	def zeros(self, shape): return tensor(np.zeros(shape))
 	def ones(self, shape): return tensor(np.ones(shape))
@@ -30,7 +42,12 @@ class tensor:
 	def __len__(self): return len(self.data)
 	def __iter__(self): return iter(self.data)
 	def __copy__(self): return tensor(self.data.copy())
-	def __eq__(self, other) -> bool: return self.data.all() == other.data.all()
-	def __ne__(self, other) -> bool: return self.data.all() != other.data.all()
+	def __eq__(self, other) -> bool: return self.data.all() == other.data.all() if type(other) == tensor else self.data.all() == other
+	def __ne__(self, other) -> bool: return self.data.all() != other.data.all() if type(other) == tensor else self.data.all() != other
 	def mean(self, axis=None): return np.mean(self.data, axis=axis)
 	def sum(self, axis=None): return np.sum(self.data, axis=axis)
+
+	def autograd(self, input):
+		if self.ops[0] == 'init': return tensor(input)
+		if self.ops[0] == 'add': return self.autograd(tensor(self, ops=self.ops[1])) + self.autograd(tensor(self, ops=self.ops[2]))
+		# if self.ops[0] == 'mul': return self.autograd(tensor(self, ops=self.ops[1])) * self.ops[3] + self.ops[3] * self.autograd(tensor(self, ops=self.ops[2]))
